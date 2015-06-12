4c9a7baf8604745a57c74495e4caf3165f84b198
Cantya Dhanurendra <cantya.dhanurendra@gmail.com>
2015-06-12 20:52:36 +0700

    menampiklan dengan mengeksekusi dahili di dalam begin

---
+++ cantya_dh/3_syntax.rb
@@ -0,0 +1,10 @@
+puts "Nama	: Cantya Dhanurendra"
+puts "NIM	: 1300018086"
+
+#BEGIN disini akan menampilkan perintah di dalamnya 
+#untuk dieksekusi pertama kali
+BEGIN {
+   puts "Biodata"
+}
+
+puts "Kelas	: C"
