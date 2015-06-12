824704383c8e497fabd3798365fccc99a399cfb2
Cantya Dhanurendra <cantya.dhanurendra@gmail.com>
2015-06-12 21:31:24 +0700

    membuat class Cantya

---
+++ class/1_class.rb
@@ -0,0 +1,10 @@
+#membuat class dengan nama Cantya
+class Cantya
+   def halo
+      puts "Hello saya CANTYA"
+   end
+end
+
+#memanggial class yang sudah dibuat tadi
+object = Cantya. new
+object.halo
