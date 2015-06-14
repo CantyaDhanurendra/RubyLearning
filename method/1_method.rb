66ca7d314bbbf0689a21b42c68e748dc2403df65
Cantya Dhanurendra <cantya.dhanurendra@gmail.com>
2015-06-14 19:05:45 +0700

    method1

---
+++ method/1_method.rb
@@ -0,0 +1,11 @@
+#menentukan nialai/kondisi awal
+def test(a1="bb", a2="cc", a3="aa")
+
+#menampilkan 
+   puts "ini #{a1}"
+   puts "some #{a2}"
+   puts "this is #{a3}"
+end
+
+test "C", "C++", "bb"
+test
